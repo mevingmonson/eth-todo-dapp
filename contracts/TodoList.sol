0887826601dd47fa02987ae124b8986ad518de38
Mevin G Monson <mevingmonson@gmail.com>
2021-11-18 00:09:17 +0530

    createTask method developed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping (uint => Task) public Tasks;
+    
+constructor () public {
+    createTask("hey you!");
+}
+
+    function createTask( string memory _content) public {
+        taskCount++;
+        Tasks[taskCount]=Task(taskCount,_content,false);
+    }
 }
